ad6e1cb7e4f16a2f81852f1e8c4fdd9b88b69385
Cristian J Benitez <cristianjbenitez@gmail.com>
2022-08-29 17:39:07 -0300

    Agregado de menu, metodos para mostrar y logica de opciones

--- app/src/main/java/com/utn/tp2_grupo7_pa2/MainActivity.java
+++ app/src/main/java/com/utn/tp2_grupo7_pa2/MainActivity.java
@@ -3,6 +3,9 @@ package com.utn.tp2_grupo7_pa2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //mostrar u ocultar menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_app, menu);
+        return true;
+    }
+
+    //asigna funciones a las opciones del menu
+    public boolean onOptionsItemSelected(MenuItem item){
+        int idOpcion = item.getItemId();
+
+        if(idOpcion == R.id.it_agregar){
+            Toast.makeText(this,"agregar",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Listar",Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
