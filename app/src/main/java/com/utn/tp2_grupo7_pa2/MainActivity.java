7909a15cca6d84639c9a17bc1728034c65aeea19
Cristian J Benitez <cristianjbenitez@gmail.com>
2022-08-29 16:18:57 -0300

    primer commit proyecto

---
+++ app/src/main/java/com/utn/tp2_grupo7_pa2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.utn.tp2_grupo7_pa2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
